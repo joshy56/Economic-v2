8310a135745802e0d2779999821d7cee2aa0f0ef
joshy56 <rossi_agustin_2003@hotmail.com>
2024-03-11 12:30:51 -0300

    Add static methods on Response

--- src/main/java/io/github/joshy56/response/Response.java
+++ src/main/java/io/github/joshy56/response/Response.java
@@ -1,6 +1,7 @@
 package io.github.joshy56.response;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
@@ -9,6 +10,22 @@ import java.util.Optional;
  * @since 3/2/2024
  */
 public record Response<T>(@NotNull ResponseCode responseCode, Optional<Throwable> message, Optional<T> response) {
+    public static <V> Response<V> OK(@Nullable V response) {
+        return new Response<>(ResponseCode.OK, Optional.empty(), Optional.ofNullable(response));
+    }
+
+    public static Response<Void> EMPTY() {
+        return new Response<>(ResponseCode.OK, Optional.empty(), Optional.empty());
+    }
+
+    public static <V> Response<V> ERROR(@Nullable Throwable throwable) {
+        return new Response<>(ResponseCode.ERROR, Optional.ofNullable(throwable), Optional.empty());
+    }
+
+    public static Response<Void> NOT_IMPLEMENTED() {
+        return new Response<>(ResponseCode.NOT_IMPLEMENTED, Optional.empty(), Optional.empty());
+    }
+
     public boolean failed() {
         return (responseCode.equals(ResponseCode.NOT_IMPLEMENTED) || responseCode.equals(ResponseCode.ERROR));
     }
